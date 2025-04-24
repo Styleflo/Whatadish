b2794edee11108dc5048e63a5ea05372c985a80f
Florian Touraine <florian.touraine@gmail.com>
2025-04-24 22:30:22 +0200

    merge de main into la_db

--- helper/csvToDb.py
+++ helper/csvToDb.py
@@ -2,12 +2,12 @@ import pandas as pd
 import sqlite3
 
 # Charger le CSV
-df = pd.read_csv("concap.csv")
+df = pd.read_csv("../static/Data/bd1.csv")
 
 # Connexion à une base SQLite (créée si elle n'existe pas)
-conn = sqlite3.connect("my_base.db")
+conn = sqlite3.connect("../my_base.db")
 
 # Exporter le DataFrame dans la base sous forme de table
-df.to_sql("CountryTable", conn, if_exists="replace", index=False)
+df.to_sql("my_table", conn, if_exists="replace", index=False)
 
 conn.close()
